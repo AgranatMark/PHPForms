b9b3844af07d1dcfa4f7d68041161eb52f1eddd8
Mark <agranat_mark@mail.ru>
2015-12-11 02:19:25 +0500

    First Commit

---
+++ form.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+</HEAD>
+<BODY>
+<form action="form_script.php" method="POST">
+
+<p>Name: <input type="text" name="name" size="30"/><p>
+
+<p>Size: <select name="size">
+<option value="small">Small</option>
+<option value="medium">Medium</option>
+<option value="large">Large</option>
+</select></p>
+
+<input type="submit" name="submit" value="Submit Button"/>
+</BODY>
+</HTML>
